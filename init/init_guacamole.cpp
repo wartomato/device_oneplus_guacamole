4872cc6e3f9361050d7a5c33fef24b0ab1016070
Ralf Luther <wartomato@users.noreply.github.com>
2022-11-20 23:05:12 +0100

    Revert "guacamole: adjust init"
    This reverts commit 1711a138d3233f2e28da37cc877172ab33aa5732.

--- init/init_guacamole.cpp
+++ init/init_guacamole.cpp
@@ -144,11 +144,18 @@ void vendor_load_properties()
         }
     }
     property_override("ro.boot.flash.locked", "1");
+    property_override("ro.boot.selinux", "enforcing");
     property_override("ro.boot.vbmeta.device_state", "locked");
     property_override("ro.boot.verifiedbootstate", "green");
     property_override("ro.boot.veritymode", "enforcing");
     property_override("ro.boot.warranty_bit", "0");
-    property_override("ro.warranty_bit", "0");    
+    property_override("ro.build.release_type", "release");
+    property_override("ro.build.tags", "release-keys");
+    property_override("ro.build.type", "user");
+    property_override("ro.secure", "1");
+    property_override("ro.vendor.build.release_type", "release");
+    property_override("ro.vendor.build.type", "user");
+    property_override("ro.warranty_bit", "0");
     // dalvikvm props
     load_dalvikvm_properties();
 }
