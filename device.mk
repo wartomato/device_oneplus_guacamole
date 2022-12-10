009d3158b521c58195ac31b5f640041df923ea13
Ralf Luther <luther.ralf@gmail.com>
2022-12-10 16:10:30 +0100

    guacamole: move ih8sn.conf away from common

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.oplus.hw.rc
 
+# ih8sn config(s)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-aicp
