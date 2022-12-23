c0346384c19e87a6993c3c4ce45817cd0904aeb1
Ralf Luther <luther.ralf@gmail.com>
2022-12-23 14:45:13 +0100

    guacamole: explicitly enforce all RRO targets

--- device.mk
+++ device.mk
@@ -35,6 +35,8 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-aicp
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
