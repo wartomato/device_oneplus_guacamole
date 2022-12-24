deb0a080c8fbb2544928e325b98e565bbea4e5db
Ralf Luther <luther.ralf@gmail.com>
2022-12-24 14:53:35 +0100

    Revert "guacamole: [TEST] convert aicp overlays to RRO"
    This reverts commit c394eab99299b1b251f3d5e1c1dd87d2c9e6f2f4.

--- device.mk
+++ device.mk
@@ -32,12 +32,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
 
 # Overlays
-PRODUCT_PACKAGES += \
-    AICPFrameworksResTarget \
-    AICPSystemUIResTarget \
-    ApertureResTarget \
-    OPlusExtrasResTarget \
-    OPlusDozeTarget
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-aicp
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
