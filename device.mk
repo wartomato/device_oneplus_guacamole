410a19078a061300abf0d2c86a524a04136a834a
Ralf Luther <luther.ralf@gmail.com>
2023-01-29 17:11:35 +0100

    guacamole: remove ih8sn config

--- device.mk
+++ device.mk
@@ -27,10 +27,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom
 
-# ih8sn config(s)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
