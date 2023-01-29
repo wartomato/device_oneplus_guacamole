031bc3151537899d0414cc2530bd8dc6ebdb17c3
Ralf Luther <luther.ralf@gmail.com>
2023-01-29 16:45:09 +0100

    guacamole: I WANT MY INIT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,10 @@ DEVICE_PATH := device/oneplus/guacamole
 # Display
 TARGET_SCREEN_DENSITY := 560
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_guacamole
+TARGET_RECOVERY_DEVICE_MODULES := libinit_guacamole
+
 # Partitions
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
