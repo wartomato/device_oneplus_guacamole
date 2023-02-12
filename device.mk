bc2ed5aaadfb264ab2be4461eb5a283b84116cb7
Ralf Luther <luther.ralf@gmail.com>
2023-02-12 17:59:36 +0100

    guacamole: cleanup and move overlays

--- device.mk
+++ device.mk
@@ -29,11 +29,10 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aicp
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
+    OPlusPartsResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
